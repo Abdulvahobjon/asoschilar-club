d00be534b419c60a62249e730f23fbbcd1f2dc26
abdulvahob0714@gmail.com <dilbarxonxiziraliyeva@gmail.com>
2024-12-16 20:40:39 +0500

    add

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 let headerResponsiveClose = document.querySelector(".header-responsive-close");
 let headerResponsive = document.querySelector(".header-responsive");
+let headerResponsiveNavLink = document.querySelectorAll(".header-responsive .nav-link");
 let headerHamburger = document.querySelector(".header-hamburger");
 
 headerResponsiveClose.addEventListener('click' , function(){
@@ -10,4 +11,11 @@ headerResponsiveClose.addEventListener('click' , function(){
 headerHamburger.addEventListener('click' , function(){
     headerResponsive.classList.toggle("active")
     document.body.classList.toggle("active")
+})
+
+headerResponsiveNavLink.forEach(function(item , index){
+    item.addEventListener('click' , function(){
+        headerResponsive.classList.toggle("active")
+        document.body.classList.toggle("active")
+    })
 })
